af1b81be234fd406053d642810eace8dbe130ccf
krsitistisr <kr.kuus@gmail.com>
2024-02-25 13:59:17 +0200

    data doesnt work

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("Nothing? Really?");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value === "";
+    saveData();
+}
+listContainer.addEventListener("click", function(e){
+    console.log("Event Target:", e.target); 
+
+    if (e.target.tagName === "LI") {
+        console.log("Clicked LI");
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        console.log("Clicked SPAN");
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// listContainer.addEventListener("click", function(e){
+//     if(e.target.tagName === "LI"){
+//         e.target.classList.toggle("checked");
+//         saveData();
+//     } else if(e.target.tagname === "SPAN"){
+//         e.target.parentElement.remove();
+//         saveData();
+//     }
+// }, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHtml);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getitem("data");
+
+}
+showTask();
